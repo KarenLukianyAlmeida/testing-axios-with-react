659fc7f07c6de6f3816ce21dad72654e3e7c5835
Karen Almeida <karenalmeida.lukiany@gmail.com>
2025-03-31 22:22:35 +0100

    Feat: Implement useEffect to render initial image in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import { Button } from "@mui/material";
 import "./App.css";
 import InfoCard from "./components/InfoCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getCatImage } from "./services/catAPI";
 import ErrorMessage from "./components/ErrorMessage";
 
 function App() {
   const [infoCat, setInfoCat] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const getInitialData = async () => {
+      try {
+        const catImage = await getCatImage();
+        setInfoCat(catImage);
+      } catch (err) {
+        setError(err as string);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getInitialData();
+  }, []);
 
   const handleClick = async () => {
+    setLoading(true);
+    setError(null);
     try {
       const imageUrl = await getCatImage();
       setInfoCat(imageUrl);
-    } catch (error) {
-      setError(error as string);
+    } catch (err) {
+      setError(err as string);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div>
-      {error ? (
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
         <ErrorMessage errorData={error} />
       ) : (
         infoCat && <InfoCard imageUrl={infoCat} />
